36ffd5ff5f73d99d9b9097afa506a43d57eb11af
Spootscupertino <122686531+Spootscupertino@users.noreply.github.com>
2025-11-19 22:03:53 -0800

    Update Recipe.kt

--- Recipe.kt
+++ Recipe.kt
@@ -1,1 +1,31 @@
+package com.example.heardthatsandbox.data
 
+import com.google.gson.annotations.SerializedName
+
+data class Recipe(
+    @SerializedName("id")
+    val id: String,
+
+    @SerializedName("title")
+    val title: String,
+
+    @SerializedName("category")    val category: String,
+
+    @SerializedName("difficulty")
+    val difficulty: String,
+
+    @SerializedName("prep_time")
+    val prepTime: String,
+
+    @SerializedName("cook_time")
+    val cookTime: String,
+
+    @SerializedName("image_url")
+    val imageUrl: String,
+
+    @SerializedName("ingredients")
+    val ingredients: List<String>,
+
+    @SerializedName("instructions")
+    val instructions: List<String>
+)
