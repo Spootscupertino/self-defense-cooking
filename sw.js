ab21e47d9916539f4f597b60d891faa3eea622ca
ericeldridge <eweldridge@gmail.com>
2025-11-23 14:19:10 -0800

    app mobile

---
+++ sw.js
@@ -0,0 +1,33 @@
+const CACHE_NAME = 'sdc-dojo-v1';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './style.css',
+  './script.js',
+  './manifest.json',
+  './assets/chef-inhale.png',
+  './assets/skybox/dojo-front.png',
+  './assets/skybox/dojo-back.png',
+  './assets/skybox/dojo-left.png',
+  './assets/skybox/dojo-right.png',
+  './assets/skybox/dojo-top.png',
+  './assets/skybox/dojo-bottom.png',
+  './apps/nutrition-widget/index.html',
+  './apps/nutrition-widget/ingredients.js'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
